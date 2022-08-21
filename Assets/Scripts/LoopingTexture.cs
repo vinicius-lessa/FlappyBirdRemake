fca884970e644e9a9c74c1a36a4abad83facf622
Gabriel Shirai <gabrielshiraimonteiro@gmail.com>
2022-08-20 22:42:48 -0300

    Atualizacao das animaçoes de cenario e adicao de tilemaps

--- Assets/Scripts/LoopingTexture.cs
+++ Assets/Scripts/LoopingTexture.cs
@@ -1,8 +1,21 @@
+/**
+* File DOC
+* 
+* @Description Script de gerenciamento da animação do Cenário (Background e chão).
+* 
+* @ChangeLog 
+*   - Vinícius Lessa - 08/19/2022: Criação do Arquivo e documentação de cabeçalho. 
+* 
+* @ Tips & Tricks: 
+* 
+*
+**/
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LoopingBackground : MonoBehaviour
+public class LoopingTexture : MonoBehaviour
 {
     public float backgroundSpeed;
     public Renderer backgroundRenderer;
